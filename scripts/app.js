470100d3c1b11c40a7525e5b2841307372a8f9a5
Adi <adrian.rus@live.com>
2016-04-23 17:44:58 +0100

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+$(document).ready();
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+	var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";	
+}
+
+var lat = showPosition(latitude);
+var long = showPosition(longitude);
+
